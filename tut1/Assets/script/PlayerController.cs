f0631ff4ea434f3d9400147ceef1f01a28c76a24
dasDome <dominic.steiner@student.reutlingen-university.de>
2016-10-26 13:09:20 +0200

    original prototype
    Commit1

---
+++ tut1/Assets/script/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	private int score;
+	public Text scoreText;
+	public Text winText;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		score = 0;
+		setScore ();
+		winText.text="";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate(){
+		// grab movement
+		float moveHorizontal = Input.GetAxis ("Horizontal")*2f;
+		//float moveVertical = Input.GetAxis("Vertical");
+		// constant move forward;
+		float moveVertical = 1f;
+
+		// apply movement
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		// with drag
+		//rb.AddForce (movement*speed);
+
+		// without drag
+		rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical)*speed;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("item")) {
+			other.gameObject.SetActive (false);
+			score++;
+			setScore ();
+			if (score >= 7) {
+				winText.text="Congratulations!!";
+			}
+		}
+		if (other.gameObject.CompareTag ("obstacle")) {
+			score--;
+			setScore ();
+			if (score < 0) {
+				winText.text="Fail!!";
+			}
+		}
+	}
+	void setScore(){
+		scoreText.text = "Score: " + score.ToString ();
+	}
+}
