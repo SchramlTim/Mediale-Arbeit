8d04f21a2c9f400a7f78c2b5c2baec45a2871faf
dasDome <dominic.steiner@student.reutlingen-university.de>
2016-10-30 12:56:03 +0100

    Added Hubworld, fixed HUD for different screens (prototype1.3), added scene manager

---
+++ tut1/Assets/script/sceneController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneController : MonoBehaviour {
+	//used in levels
+	public void returnToHub(){
+		SceneManager.LoadScene("Hub");
+	}
+	public void restartGame(){
+		Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+	}
+
+
+	// hubworld
+	public void loadLvl1(){
+		SceneManager.LoadScene("prototype");
+	}
+	public void loadLvl2(){
+		SceneManager.LoadScene("Lvl screens");
+	}
+	public void loadLvl3(){
+		Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+	}
+}
